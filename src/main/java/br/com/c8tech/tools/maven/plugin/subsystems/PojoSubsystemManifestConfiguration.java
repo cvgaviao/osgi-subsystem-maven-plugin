18058ccc1fdaa4dac76f3da0de95beadb968df70
Cristiano V. Gavião <cvgaviao@gmail.com>
2019-06-07 00:24:36 -0300

    upgrade to new parent pom version
    changed license headers

--- src/main/java/br/com/c8tech/tools/maven/plugin/subsystems/PojoSubsystemManifestConfiguration.java
+++ src/main/java/br/com/c8tech/tools/maven/plugin/subsystems/PojoSubsystemManifestConfiguration.java
@@ -1,15 +1,13 @@
 /**
- * ==========================================================================
- * Copyright © 2015-2018 Cristiano Gavião, C8 Technology ME.
+ * ============================================================================
+ *  Copyright ©  2015-2019,    Cristiano V. Gavião
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ *  All rights reserved.
+ *  This program and the accompanying materials are made available under
+ *  the terms of the Eclipse Public License v1.0 which accompanies this
+ *  distribution and is available at http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors:
- * Cristiano Gavião (cvgaviao@c8tech.com.br)- initial API and implementation
- * ==========================================================================
+ * ============================================================================
  */
 package br.com.c8tech.tools.maven.plugin.subsystems;
 
